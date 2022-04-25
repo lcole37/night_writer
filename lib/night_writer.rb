9d5ec5bbc1cfca98a07aadb221b3d3428282788a
Lucas Colwell <colwellc37@gmail.com>
2022-04-25 13:41:47 -0600

    Feat: add translator class, start dictionary method

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,13 +3,3 @@ p "Created #{ARGV[1]} containing #{input_file.read.length} characters"
 
 # p File.read(input_file)
 File.write('./braille.txt', File.read(input_file))
-
-
-# input_message = input_file.read
-# input_file.close
-# character_length = input_file.length
-
-# writer = File.write(ARGV[1], input_file.gsub("256", "#{character_length}"))
-# writer.write(input_message.reverse)
-# writer.close
-#
---
+++ spec/translator_spec.rb
@@ -0,0 +1,29 @@
+require './lib/translator'
+
+describe Translator do
+
+  it "exists" do
+
+  end
+
+  it 'has attributes' do
+
+  end
+
+  xit 'can translate single characters' do
+
+  end
+
+  xit 'can translate a string of characters' do
+
+  end
+
+  xit 'can translate spaces and punctuation' do
+
+  end
+
+  # xit 'can translate capitals' do
+  #
+  # end
+
+end
