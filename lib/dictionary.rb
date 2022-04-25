4b4bd8e512d45ef5f3c11acdc62fe4e9211d0489
Lucas Colwell <colwellc37@gmail.com>
2022-04-25 15:55:46 -0600

    Feat: finish translate_string

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,43 +1,51 @@
+require 'pry'
 class Dictionary
-  attr_reader :text,
-
-
-  def initialize(text)
-    @text = text
-  end
 
   def english_braille_dictionary
     translation_hash = {
-      a: '0. .. ..',   #these need gsub '', '/n'
-      b: '0. 0. ..',
-      c: '00 .. ..',
-      d: '00 .0 ..',
-      e: '0. .0 ..',
-      f: '00 0. ..',
-      g: '00 00 ..',
-      h: '0. 00 ..',
-      i: '.0 0. ..',
-      j: '.0 00 ..',
-      k: '0. .. 0.',
-      l: '0. 0. 0.',
-      m: '00 .. 0.',
-      n: '00 .0 0.',
-      o: '0. .0 0.',
-      p: '00 0. 0.',
-      q: '00 00 0.',
-      r: '0. 00 0.',
-      s: '.0 0. 0.',
-      t: '.0 00 0.',
-      u: '0. .. 00',
-      v: '0. 0. 00',
-      w: '.0 00 .0',
-      x: '00 .. 00',
-      y: '00 .0 00',
-      z: '0. .0 00',
+      'a' => ['0.', '..', '..'],
+      'b' => ['0.', '0.', '..'],
+      'c' => ['00', '..', '..'],
+      'd' => ['00', '.0', '..'],
+      'e' => ['0.', '.0', '..'],
+      'f' => ['00', '0.', '..'],
+      'g' => ['00', '00', '..'],
+      'h' => ['0.', '00', '..'],
+      'i' => ['.0', '0.', '..'],
+      'j' => ['.0', '00', '..'],
+      'k' => ['0.', '..', '0.'],
+      'l' => ['0.', '0.', '0.'],
+      'm' => ['00', '..', '0.'],
+      'n' => ['00', '.0', '0.'],
+      'o' => ['0.', '.0', '0.'],
+      'p' => ['00', '0.', '0.'],
+      'q' => ['00', '00', '0.'],
+      'r' => ['0.', '00', '0.'],
+      's' => ['.0', '0.', '0.'],
+      't' => ['.0', '00', '0.'],
+      'u' => ['0.', '..', '00'],
+      'v' => ['0.', '0.', '00'],
+      'w' => ['.0', '00', '.0'],
+      'x' => ['00', '..', '00'],
+      'y' => ['00', '.0', '00'],
+      'z' => ['0.', '.0', '00'],
+      ' ' => ['..', '..', '..']
     }
   end
 
   def translate(input)
-    input == english_braille_dictionary.keys.any?
+    t = english_braille_dictionary[input]
   end
+
+  def translate_string(input)
+    string = []
+    input.chars.each do |char|
+      if english_braille_dictionary.keys.include?(char)
+        string << translate(char)
+      end
+    end
+    return string
+  end
+  
+  #to print braille, enumerate each (of string) letter[0], /n, letter[1], /n letter[3]
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -2,21 +2,49 @@ require 'rspec'
 require './lib/dictionary'
 
 RSpec.describe Dictionary do
+  before :each do
+    @dictionary = Dictionary.new
+  end
 
   it "exists" do
-
+    expected_hash = {
+      'a' => ['0.', '..', '..'],
+      'b' => ['0.', '0.', '..'],
+      'c' => ['00', '..', '..'],
+      'd' => ['00', '.0', '..'],
+      'e' => ['0.', '.0', '..'],
+      'f' => ['00', '0.', '..'],
+      'g' => ['00', '00', '..'],
+      'h' => ['0.', '00', '..'],
+      'i' => ['.0', '0.', '..'],
+      'j' => ['.0', '00', '..'],
+      'k' => ['0.', '..', '0.'],
+      'l' => ['0.', '0.', '0.'],
+      'm' => ['00', '..', '0.'],
+      'n' => ['00', '.0', '0.'],
+      'o' => ['0.', '.0', '0.'],
+      'p' => ['00', '0.', '0.'],
+      'q' => ['00', '00', '0.'],
+      'r' => ['0.', '00', '0.'],
+      's' => ['.0', '0.', '0.'],
+      't' => ['.0', '00', '0.'],
+      'u' => ['0.', '..', '00'],
+      'v' => ['0.', '0.', '00'],
+      'w' => ['.0', '00', '.0'],
+      'x' => ['00', '..', '00'],
+      'y' => ['00', '.0', '00'],
+      'z' => ['0.', '.0', '00'],
+      ' ' => ['..', '..', '..']
+    }
+    expect(@dictionary.english_braille_dictionary).to eq(expected_hash)
   end
 
-  it 'has attributes' do
-
+  it 'can translate single characters' do
+    expect(@dictionary.translate('l')).to eq(['0.', '0.', '0.'])
   end
 
-  xit 'can translate single characters' do
-
-  end
-
-  xit 'can translate a string of characters' do
-
+  it 'can translate a string of characters' do
+    expect(@dictionary.translate_string('abc')).to eq([['0.', '..', '..'], ['0.', '0.', '..'], ['00', '..', '..']])
   end
 
   xit 'can translate spaces and punctuation' do
